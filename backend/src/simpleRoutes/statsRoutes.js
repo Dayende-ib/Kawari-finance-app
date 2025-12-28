fbe32bf636f45fd251857a051718ebe132eb92b2
Ougda Ibrahim Dayende <o.ibrahimdayende@gmail.com>
2025-12-28 14:45:00 +0000

    chore: Frontend Uniquement

--- backend/src/simpleRoutes/statsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ctrl = require('./statsController');
-const auth = require('./authMiddlewareSimple');
-
-router.get('/', auth, ctrl.getStats);
-
-module.exports = router;
